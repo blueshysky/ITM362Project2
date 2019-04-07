70db058c4e81a7a61b12332d896944082b685fc4
Matthew Hofmann <mhofmann@hawk.iit.edu>
2019-04-07 17:08:23 -0500

    Add listeners for buttons

--- order/site.js
+++ order/site.js
@@ -4,3 +4,13 @@ var b=document.getElementsByClassName("dropcontent")[0];
 document.getElementById('dropdown').addEventListener("click", function(event) {
 b.classList.toggle("visible");
 })
+//listener for specialty pizza button
+var s=document.getElementsByClassName('pizzas')[0];
+document.getElementById('special').addEventListener("click", function(event) {
+  s.classList.toggle("visible2");
+})
+//listener for build your own button
+var u=document.getElementsByClassName('builds')[0];
+document.getElementById('build').addEventListener("click", function(event) {
+  u.classList.toggle("visible3");
+})

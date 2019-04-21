30c652ab56d14b8f5477db922cbeac852444eb10
blueshysky <xionghuih745@gmail.com>
2019-04-20 20:03:06 -0500

    changed the function so that the tabs display which ever the person clicked

--- info/site.js
+++ info/site.js
@@ -8,28 +8,46 @@ document.addEventListener("DOMContentLoaded", function(){
   var content_summary = document.querySelector("#summary_content");
   var content_payment = document.querySelector("#payments_content");
 
+
+
   payment_tab.addEventListener("click", function(){
-    if(content_summary.classList.contains(" ")) {
-    content_summary.classList.add("hidden");
-    console.log("Someone clicked on payment tab");
-    if(content_payment.classList.contains("hidden") && content_summary.classList.contains("show")){
-      content_payment.classList.remove("hidden")
-    }
-    }else
-    {
-    payments_content.classList.add("show");
-    }
+    // var summary_click = summary_tab.addEventListener("click", function(){
+    //   return true;
+    // });
+    console.log("clicked");
+
+    summary_content.classList.add("hidden");
+    content_payment.classList.remove("hidden");
+
+    // if(content_payment.classList.contains("hidden") && summary_click == true){
+    //   content_payment.classList.remove("hidden");
+    // };
+
 
   });
 
   summary_tab.addEventListener("click", function(){
-    if(payments_content.classList.contains(" ")) {
-    payments_content.classList.add("hidden");
-    console.log("Someone clicked on payment tab");
-    }else
-    {
-    payments_content.classList.add("show");
-    }
+    // if(payments_content.classList.contains(" ")) {
+    // payments_content.classList.add("hidden");
+    // console.log("Someone clicked on payment tab");
+    // }else
+    // {
+    // payments_content.classList.add("show");
+    // }
+
+    console.log("clicked");
+
+    // var payment_click = payment_tab.addEventListener("click", function(){
+    //   return true;
+    // });
+
+    content_payment.classList.add("hidden");
+    content_summary.classList.remove("hidden");
+
+    // if(content_summary.classList.contains("hidden") && payment_click == true){
+    //   content_summary.classList.remove("hidden");
+    //   payments_content.classList.add("hidden");
+    // }
 
 });
 
